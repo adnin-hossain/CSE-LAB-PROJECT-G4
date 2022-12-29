b936665ceacf35dbad21b8543123cc16a5b15329
MissMahabuba <119802862+MissMahabuba@users.noreply.github.com>
2022-12-29 14:44:38 +0600

    Update main.c

--- main.c
+++ main.c
@@ -428,3 +428,78 @@ void restaurants(int restaurant_choice)
         }
     }
 }
+void search_by_food()
+{
+    total = 0;
+
+    while (1) {
+        printf("\n\nPlease choose the ");
+        printf("food\n\n1) %s\t%d\n2) %s\t%d",
+               m[1].first_food, m[1].first,
+               m[1].second_food, m[1].second);
+
+        printf("\n3) %s\t%d\n4) %s\t%d\n",
+               m[1].third_food, m[1].third,
+               m[2].first_food, m[2].first);
+
+        printf("5) %s\t%d\n6) %s\t%d\n",
+               m[2].second_food, m[2].second,
+               m[2].third_food, m[2].third);
+
+        printf("7) %s\t%d\n8) %s\t%d\n",
+               m[3].first_food, m[3].first,
+               m[3].second_food, m[3].second);
+
+        printf("9) %s\t%d\n10) Cart\n",
+               m[3].third_food,
+               m[3].third);
+        printf("11) Exit");
+        printf("\nPlease Enter Your Choice\t");
+        scanf("%d", &food);
+        if (food > 10) {
+            printf("Please enter the valid choice\n\n");
+            search_by_food();
+        }
+
+        else if (food == 10)
+            cart();
+        else if (food == 11)
+            return;
+
+        else
+            food_order(food);
+    }
+}
+
+
+void food_order(int food)
+{
+    if (food >= 1 && food <= 3)
+        restaurant_id = 1;
+    else if (food >= 4 && food <= 6)
+        restaurant_id = 2;
+    else
+        restaurant_id = 3;
+    if ((food % 3) == 1) {
+        printf("Please Enter the");
+        printf(" Count of %s\t",
+               m[restaurant_id].first_food);
+        scanf("%d", &n);
+        total = total + (n * m[restaurant_id].first);
+    }
+    else if ((food % 3) == 2) {
+        printf("Please Enter the ");
+        printf("Count of %s\t",
+               m[restaurant_id].second_food);
+        scanf("%d", &n);
+        total = total + (n * m[restaurant_id].second);
+    }
+    else if ((food % 3) == 0) {
+        printf("Please Enter the ");
+        printf("Count of %s\t",
+               m[restaurant_id].third_food);
+        scanf("%d", &n);
+        total = total + (n * m[restaurant_id].third);
+    }
+}
+
