55540b66a7446849be86630bd47495cd8b768c5e
Samiagitt <119676717+Samiagitt@users.noreply.github.com>
2022-12-29 14:21:31 +0600

    Update main.c

--- main.c
+++ main.c
@@ -106,5 +106,115 @@ void signup()
          login();
     }
 }
+int validate()
+{
 
+    for (i = 0; temp_name[i] != '\0'; i++) {
+        if (!((temp_name[i] >= 'a' && temp_name[i] <= 'z')
+              || (temp_name[i] >= 'A'
+                  && temp_name[i] <= 'Z'))) {
+            printf("\nPlease Enter the valid Name\n");
+            flag = 0;
+            break;
+        }
+    }
+    if (flag == 1) {
+        count = 0;
+
+
+        for (i = 0;temp_email[i] != '\0'; i++) {
+            if (temp_email[i] == '@'|| temp_email[i] == '.')
+                count++;
+        }
+        if (count >= 2&& strlen(temp_email) >= 5) {
+
+            if (!strcmp(temp_password1,
+                        temp_password2)) {
+                if (strlen(temp_password1) >= 8
+                    && strlen(temp_password1) <= 12) {
+                    caps = 0;
+                    small = 0;
+                    numbers = 0;
+                    special = 0;
+                    for (i = 0; temp_password1[i] != '\0';
+                         i++) {
+                        if (temp_password1[i] >= 'A'
+                            && temp_password1[i] <= 'Z')
+                            caps++;
+                        else if (temp_password1[i] >= 'a'
+                                 && temp_password1[i]
+                                        <= 'z')
+                            small++;
+                        else if (temp_password1[i] >= '0'
+                                 && temp_password1[i]
+                                        <= '9')
+                            numbers++;
+                        else if (temp_password1[i] == '@'
+                                 || temp_password1[i] == '&'
+                                 || temp_password1[i] == '#'
+                                 || temp_password1[i]
+                                        == '*')
+                            special++;
+                    }
+                    if (caps >= 1 && small >= 1
+                        && numbers >= 1 && special >= 1) {
+
+                        if (temp_age != 0 && temp_age > 0) {
+
+                            if (strlen(temp_mobile) == 11) {
+                                for (i = 0; i < 10; i++) {
+                                    if (temp_mobile[i]
+                                            >= '0'
+                                        && temp_mobile[i]
+                                               <= '10') {
+                                        success = 1;
+                                    }
+                                    else {
+                                        printf("\n\nPlease Enter Valid 11 digit mobile Number\n\n");
+                                        return 0;
+                                        break;
+                                    }
+                                }
+
+
+                                if (success == 1)
+                                    return 1;
+                            }
+                            else {
+                                printf("\n\nPlease Enter the 11 digit mobile number\n");
+                                return 0;
+                            }
+                        }
+                        else {
+                            printf("\n\nPlease Enter the valid age\n\n");
+                            return 0;
+                        }
+                    }
+                    else {
+                        printf("\n\nPlease Enter the");
+                        printf("strong password, Your ");
+                        printf("password must contain ");
+                        printf("atleast one uppercase, ");
+                        printf("Lowercase, Number and ");
+                        printf("special character\n\n");
+                        return 0;
+                    }
+                }
+                else {
+                    printf("\nYour Password is very short\nLength must between 8 to 12\n\n");
+                    return 0;
+                }
+            }
+            else {
+                printf("\nPassword Mismatch\n\n");
+                return 0;
+            }
+        }
+        else {
+            printf("\nPlease Enter Valid E-Mail\n\n");
+
+            return 0;
+        }
+    }
+}
 
